6caafba75f5762df00f79ebf2e89ffbc289c7e82
ericndungutse <dav.ndungutse@gmail.com>
2025-01-15 20:48:57 +0200

    add nested routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,13 @@ import inter from './ui/fonts';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {/* Alert */}
+        <div className='bg-yellow-400 text-black p-2 text-center'>
+          <p>Alert: Your account is due for renewal.</p>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
